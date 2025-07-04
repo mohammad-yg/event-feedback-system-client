11b785aaa6fe65ae28ac1f1596d70198d4c88417
mohammad-yg <mohammadyadegari032@gmail.com>
2025-07-04 13:23:03 +0330

    feat: authorize support in useApi hook

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,7 +1,16 @@
+import { SERVER_BASE_URL } from 'src/lib/services/services-config'
 import useSWR from 'swr'
 
-const fetcher = async <T,>(url: string): Promise<T> => {
-    const res = await fetch(url)
+const fetcher = async <T,>(url: string, accessToken: string | undefined = undefined): Promise<T> => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const headers: any = {}
+
+    if (accessToken)
+        headers['Authorization'] = `Bearer ${accessToken}`
+
+    const res = await fetch(SERVER_BASE_URL + url, {
+        headers: headers
+    })
     if (!res.ok) {
         const error = new Error('An error occurred while fetching the data.')
         throw error
@@ -9,8 +18,8 @@ const fetcher = async <T,>(url: string): Promise<T> => {
     return res.json()
 }
 
-export function useApi<T>(url: string | null) {
-    return useSWR<T>(url, fetcher, {
+export function useApi<T>(url: string | null, accessToken: string | undefined = undefined) {
+    return useSWR<T>(url, (url: string) => fetcher(url, accessToken), {
         revalidateOnFocus: false,
         shouldRetryOnError: false
     })
