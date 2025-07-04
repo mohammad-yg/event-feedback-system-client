8124b19ae143095bfad5511471f4f9c4be234542
mohammad-yg <mohammadyadegari032@gmail.com>
2025-07-04 13:41:24 +0330

    refactor(events): refactor events list page

--- src/app/(ashboard)/events/page.tsx
+++ src/app/(ashboard)/events/page.tsx
@@ -1,25 +1,19 @@
 "use client";
+
 import { useApi } from "src/hooks/useApi";
 import { EventCard, EventCardSkeleton } from "src/components/events/event-card";
-import { SERVER_BASE_URL } from "src/lib/services/services-config";
+import type { IEvent } from "src/types/event-type";
 
-interface Event {
-  id: number;
-  title: string;
-  description: string;
-  location: string;
-  dateTime: string;
-}
+type Response = {
+  data: IEvent[];
+};
 
-export default function EventsPage() {
-  const {
-    data,
-    error,
-    isLoading,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } = useApi<any>(SERVER_BASE_URL + "/api/events?page=1&pageCount=10");
-
-  const events = (data?.data as Event[]) ?? [];
+const EventsListPage = () => {
+  //fetch events
+  const { data, error, isLoading } = useApi<Response>(
+    "/api/events?page=1&pageCount=10"
+  );
+  const events = data?.data ?? [];
 
   if (error) return <div>Failed to load events</div>;
 
@@ -28,17 +22,27 @@ export default function EventsPage() {
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-3xl font-bold">Events</h1>
       </div>
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {isLoading ? (
-          <>
-            <EventCardSkeleton />
-            <EventCardSkeleton />
-            <EventCardSkeleton />
-          </>
-        ) : (
-          events?.map((event) => <EventCard key={event.id} event={event} />)
-        )}
-      </div>
+      {error ? (
+        <div className="text-center py-8 text-destructive">
+          Failed to load events
+        </div>
+      ) : (
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {isLoading ? (
+            <>
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+            </>
+          ) : (
+            events?.map((event) => (
+              <EventCard key={event.id} event={event} />
+            ))
+          )}
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default EventsListPage;
