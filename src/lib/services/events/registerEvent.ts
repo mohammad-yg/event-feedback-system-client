c932b58af4b96db4566c030a9e74b9cd250d672a
mohammad-yg <mohammadyadegari032@gmail.com>
2025-07-04 13:32:06 +0330

    refactor: improve registerEventService argument type

--- src/lib/services/events/registerEvent.ts
+++ src/lib/services/events/registerEvent.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ServiceResult } from "../service-result";
 import { SERVER_BASE_URL } from "../services-config";
 
-export const registerEventService = async (eventId: number, token: string): Promise<ServiceResult> => {
+export const registerEventService = async (eventId: number | string, token: string): Promise<ServiceResult> => {
     try {
         await axios.post(SERVER_BASE_URL + `/api/events/${eventId}/register`, {}, {
             headers: {
